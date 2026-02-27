e83c3d45a274c94d0dda4c401d8ba5a184343f2f
eternitvy <crrissant@gmail.com>
2026-02-27 07:35:05 +0700

    praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,38 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/welcome', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'Regita Abelia Putri Satriyo <br> 244107020173';
+});
+
+// Route::get('/user/{name}', function ($name) {
+//     return 'Nama Saya '. $name;
+// });
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId){
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+    });
+    
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+// Route::get('/user/{name?}', function ($name=null) {
+//     return 'Nama saya '.$name;
+// });
+
+Route::get('/user/{name?}', function ($name= 'John') {
+    return 'Nama saya '.$name;
 });
